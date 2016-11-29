b75d3179b87a8c014506c5ce6f3ba12240d62812
ahsan <kanwar.ahsan@doozielabs.com>
2016-11-29 19:10:51 +0500

    all

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,36 @@
-//alert("hello");
+
+				<!-- collapse panel script --> 
+				/* collapse */
+
+
+				function toggleChevron(e) {
+					$(e.target)
+					.prev('.panel-heading')
+					.find("i.indicator")
+				//.toggleClass('glyphicon-chevron-down glyphicon-chevron-right');
+
+
+				//.toggleClass('glyphicon-chevron-down glyphicon-chevron-up');
+			}
+			$('#accordion').on('hidden.bs.collapse', toggleChevron);
+
+			/* sub heading show hide  */
+
+			$(document).ready(function(){
+				$(".select").click(function(){
+					$('p').removeClass("hide");
+					$(this).toggleClass("hide");
+				});
+			});
+
+
+
+
+					/* Tool Tip  */
+		/*
+		$(document).ready(function(){
+		    $('[data-toggle="tooltip"]').tooltip();   
+		});*/
+
+
+		$('.example').tooltip();
